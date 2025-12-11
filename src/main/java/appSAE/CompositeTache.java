f8d54f3827cd6d26bcb0f8f7d67b91057d9535ff
leo-t-88 <leo88thomas@gmail.com>
2025-12-11 15:11:20 +0100

    [Add] Patron Composite (sauf etiquette pour le moment)

---
+++ src/main/java/appSAE/CompositeTache.java
@@ -0,0 +1,27 @@
+package appSAE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeTache extends Tache {
+    private List<Tache> content = new ArrayList<>();
+
+    public CompositeTache(String titre, String description, String date) {
+        this.titre = titre;
+        this.description = description;
+        this.date = date;
+        this.estFait = false;
+    }
+
+    public void ajouterTache(Tache tache) {
+        content.add(tache);
+    }
+
+    public void supprimerTache(Tache tache) {
+        content.remove(tache);
+    }
+
+    public List<Tache> getTaches() {
+        return content;
+    }
+}
